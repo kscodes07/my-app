7cfbb657438748d5823edbc1592117fb42bf0a24
kscodes07 <kscodes07@gmail.com>
2023-06-07 02:54:26 +0530

    build fix

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -4,8 +4,8 @@ function Description() {
   return (
     <div>
       Mathura is the home for Indian I Corps (Strike Formation)[21][22] within
-      the Indian Army's Central Command, hosting Strike I Corps headquarters in
-      a large classified area in the outskirts of the city known as Mathura
+      the Indian Armys Central Command, hosting Strike I Corps headquarters in a
+      large classified area in the outskirts of the city known as Mathura
       Cantonment (Central Command itself has its headquarters at Lucknow). It
       hosts Strike Infantry units, Air Defence units, Armoured Divisions,
       Engineer brigades, Aritillery Units and classified units of Strategic
@@ -13,7 +13,7 @@ function Description() {
       India. In 2007 during Exercise Ashwamedha, all the armoured, artillery and
       infantry divisions performed a simulation of an overall NBC
       (nuclear-chemical-biological) environment. The aim was to show operational
-      ability in high intensity, short duration and 'sudden' battles.[23]
+      ability in high intensity, short duration and sudden battles.[23]
     </div>
   );
 }
